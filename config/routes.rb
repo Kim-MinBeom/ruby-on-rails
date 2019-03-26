f49eb4fc3a5d36b58b7c8473733df33c47791865
ICT <ict07@ICTui-Mac-Pro.local>
2019-03-26 13:27:25 +0900

    mainpage_ver1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root :to=> "home#index"
+  resources :books
   resources :products
   resources :posts
   get 'home/index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_060624) do
+ActiveRecord::Schema.define(version: 2019_03_26_040725) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price", precision: 7, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
