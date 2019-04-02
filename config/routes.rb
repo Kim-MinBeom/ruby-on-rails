6ddfa4247a4620e8c93bcbfb33efbf86dff870e9
ICT <ict07@ICTui-Mac-Pro.local>
2019-04-02 12:56:07 +0900

    Active record

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :comments
+  resources :users
   root :to=> "home#index"
   resources :books
   resources :products
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_040725) do
+ActiveRecord::Schema.define(version: 2019_04_01_073151) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.decimal "price", precision: 7, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -30,6 +40,13 @@ ActiveRecord::Schema.define(version: 2019_03_26_040725) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 7, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
